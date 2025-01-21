7871cb47d3eed36738ca6168d4a0634c30dae2b6
DrAlexD <alexander.drugakov@gmail.com>
2025-01-21 18:16:45 +0300

    Added `Gateway` microservice using Spring Cloud Gateway

---
+++ server/gateway-service/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("buildsrc.convention.kotlin-jvm")
+
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.kotlin.plugin.spring)
+    alias(libs.plugins.kotlin.plugin.jpa)
+    alias(libs.plugins.kotlin.plugin.serialization)
+}
+
+dependencies {
+    implementation(libs.bundles.kotlinx.ecosystem)
+
+    implementation(libs.kotlin.reflect)
+    implementation(libs.jackson.kotlin)
+    implementation(libs.kotlin.logging)
+
+    developmentOnly(libs.spring.boot.devtools)
+
+    implementation(libs.spring.boot.webflux)
+    implementation(libs.spring.cloud.gateway)
+
+    testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.spring.boot.test)
+    testImplementation(libs.mockk)
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
+    }
+}
--- server/settings.gradle.kts
+++ server/settings.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
+include(":gateway-service")
 include(":categories-service")
 include(":defining-themes-service")
 
