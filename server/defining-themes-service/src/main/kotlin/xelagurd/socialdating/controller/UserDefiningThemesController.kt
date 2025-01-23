59972cdb172c766666a31bc3abe3848a49f22711
DrAlexD <alexander.drugakov@gmail.com>
2025-01-23 17:27:06 +0300

    Added `UserDefiningThemesController`

---
+++ server/defining-themes-service/src/main/kotlin/xelagurd/socialdating/controller/UserDefiningThemesController.kt
@@ -0,0 +1,29 @@
+package xelagurd.socialdating.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import xelagurd.socialdating.dto.UserDefiningTheme
+import xelagurd.socialdating.dto.UserDefiningThemeDetails
+import xelagurd.socialdating.service.UserDefiningThemesService
+
+@RestController
+@RequestMapping(path = ["/api/v1/defining-themes/users"], produces = ["application/json"])
+class UserDefiningThemesController(
+    private val userDefiningThemesService: UserDefiningThemesService
+) {
+
+    @GetMapping
+    fun getUserDefiningThemes(@RequestParam userCategoryIds: List<Int>): Iterable<UserDefiningTheme> {
+        return userDefiningThemesService.getUserDefiningThemes(userCategoryIds)
+    }
+
+    // TODO: Add admin privileges
+    @PostMapping
+    fun addUserDefiningTheme(@RequestBody userDefiningThemeDetails: UserDefiningThemeDetails): UserDefiningTheme {
+        return userDefiningThemesService.addUserDefiningTheme(userDefiningThemeDetails)
+    }
+}
