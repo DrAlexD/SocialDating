c89bba2e923d73c55d07af9752a22121dd9fe95c
DrAlexD <alexander.drugakov@gmail.com>
2025-01-31 18:43:26 +0300

    Added `common` module

--- server/categories-service/src/main/kotlin/xelagurd/socialdating/server/controller/CategoriesController.kt
+++ server/categories-service/src/main/kotlin/xelagurd/socialdating/server/controller/CategoriesController.kt
@@ -1,9 +1,13 @@
-package xelagurd.socialdating.controller
+package xelagurd.socialdating.server.controller
 
-import org.springframework.web.bind.annotation.*
-import xelagurd.socialdating.dto.Category
-import xelagurd.socialdating.dto.CategoryDetails
-import xelagurd.socialdating.service.CategoriesService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import xelagurd.socialdating.server.model.Category
+import xelagurd.socialdating.server.model.details.CategoryDetails
+import xelagurd.socialdating.server.service.CategoriesService
 
 @RestController
 @RequestMapping(path = ["/api/v1/categories"], produces = ["application/json"])
