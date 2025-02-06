5839a45e29fd543482b48a68b614d467b078d4a7
DrAlexD <alexander.drugakov@gmail.com>
2025-02-06 12:21:11 +0300

    errors

--- client/app/src/androidTest/java/xelagurd/socialdating/client/FakeApiService.kt
+++ client/app/src/androidTest/java/xelagurd/socialdating/client/FakeApiService.kt
@@ -1,5 +1,6 @@
 package xelagurd.socialdating.client
 
+import retrofit2.Response
 import xelagurd.socialdating.client.data.fake.FakeDataSource
 import xelagurd.socialdating.client.data.model.Category
 import xelagurd.socialdating.client.data.model.DefiningTheme
@@ -14,31 +15,36 @@ import xelagurd.socialdating.client.data.model.details.StatementDetails
 import xelagurd.socialdating.client.data.remote.ApiService
 
 class FakeApiService : ApiService {
-    override suspend fun loginUser(loginDetails: LoginDetails): User? =
-        FakeDataSource.users[0]
+    override suspend fun loginUser(loginDetails: LoginDetails): Response<User> =
+        Response.success(FakeDataSource.users[0])
 
-    override suspend fun registerUser(registrationDetails: RegistrationDetails): User? =
-        FakeDataSource.users[0]
+    override suspend fun registerUser(registrationDetails: RegistrationDetails): Response<User> =
+        Response.success(FakeDataSource.users[0])
 
-    override suspend fun getUser(userId: Int): User? =
-        FakeDataSource.users[0]
+    override suspend fun getUser(userId: Int): Response<User> =
+        Response.success(FakeDataSource.users[0])
 
-    override suspend fun getCategories(): List<Category> = FakeDataSource.categories
+    override suspend fun getCategories(): Response<List<Category>> =
+        Response.success(FakeDataSource.categories)
 
-    override suspend fun getUserCategories(userId: Int): List<UserCategory> =
-        FakeDataSource.userCategories
+    override suspend fun getUserCategories(userId: Int): Response<List<UserCategory>> =
+        Response.success(FakeDataSource.userCategories)
 
-    override suspend fun getDefiningThemes(categoryIds: List<Int>): List<DefiningTheme> =
-        FakeDataSource.definingThemes
+    override suspend fun getDefiningThemes(categoryIds: List<Int>): Response<List<DefiningTheme>> =
+        Response.success(FakeDataSource.definingThemes)
 
-    override suspend fun getUserDefiningThemes(userCategoryIds: List<Int>): List<UserDefiningTheme> =
-        FakeDataSource.userDefiningThemes
+    override suspend fun getUserDefiningThemes(userCategoryIds: List<Int>): Response<List<UserDefiningTheme>> =
+        Response.success(FakeDataSource.userDefiningThemes)
 
-    override suspend fun getStatements(definingThemeIds: List<Int>): List<Statement> =
-        FakeDataSource.statements
+    override suspend fun getStatements(definingThemeIds: List<Int>): Response<List<Statement>> =
+        Response.success(FakeDataSource.statements)
 
-    override suspend fun addStatement(statementDetails: StatementDetails): Statement? =
-        FakeDataSource.newStatement
+    override suspend fun addStatement(statementDetails: StatementDetails): Response<Statement> =
+        Response.success(FakeDataSource.newStatement)
 
-    override suspend fun addStatementReaction(statementId: Int, statementReactionDetails: StatementReactionDetails) {}
+    override suspend fun addStatementReaction(
+        statementId: Int,
+        statementReactionDetails: StatementReactionDetails
+    ): Response<Unit> =
+        Response.success(null)
 }
