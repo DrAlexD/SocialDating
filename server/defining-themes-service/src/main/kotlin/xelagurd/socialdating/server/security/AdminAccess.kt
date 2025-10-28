c83c5644ab8a64093a78e9861fbd7194659c847b
Alexander Drugakov <alexander.drugakov@gmail.com>
2025-10-28 20:23:53 +0300

    Added admin-only access to some endpoints (#135)

---
+++ server/defining-themes-service/src/main/kotlin/xelagurd/socialdating/server/security/AdminAccess.kt
@@ -0,0 +1,8 @@
+package xelagurd.socialdating.server.security
+
+import org.springframework.security.access.prepost.PreAuthorize
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@PreAuthorize("hasRole('ADMIN')")
+annotation class AdminAccess
