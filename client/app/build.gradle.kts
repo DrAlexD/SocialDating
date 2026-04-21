b42466520eb6a9cb37b141a494e6460999c40c4c
DrAlexD <alexander.drugakov@gmail.com>
2026-04-22 01:19:57 +0300

    Fix

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.plugin.compose)
     alias(libs.plugins.kotlin.plugin.serialization)
     alias(libs.plugins.devtools.ksp)
